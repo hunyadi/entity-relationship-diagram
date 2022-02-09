28a7a26d683df5ecdce6a44a011959c941d0378a
Levente Hunyadi <levente.hunyadi@instructure.com>
2022-02-09 21:30:50 +0100

    Move elastic layout into separate module

--- src/boolarray.ts
+++ src/boolarray.ts
@@ -1,4 +1,13 @@
-declare interface BooleanMatrixOps {
+/**
+ * Entity Relationship Diagram (in TypeScript)
+ * @author  Levente Hunyadi
+ * @version 1.0
+ * @remarks Copyright (C) 2022 Levente Hunyadi
+ * @remarks Licensed under MIT, see https://opensource.org/licenses/MIT
+ * @see     https://hunyadi.info.hu/
+ **/
+
+export declare interface BooleanMatrixOps {
     get(i: number, j: number): boolean;
     set(i: number, j: number, value: boolean): void;
 }
--- src/erdiagram.ts
+++ src/erdiagram.ts
@@ -7,23 +7,10 @@
  * @see     https://hunyadi.info.hu/
  **/
 
-import { BooleanMatrix } from "./boolarray";
 import { Arrow, Diagram, Movable } from "./diagram";
-import { getOffsetRect } from "./htmldom";
-import { Point, Size, Vector } from "./geometry";
+import { ElasticLayout, ElasticLayoutOptions } from "./elastic";
 import TabPanel from "./tabpanel";
 
-function withDefaults<T>() {
-    return function <TDefaults extends Partial<T>>(defs: TDefaults) {
-        return function (p: Pick<T, Exclude<keyof T, keyof TDefaults>> & Partial<TDefaults>): T {
-            let result: any = p;
-            for (let k of Object.keys(defs)) {
-                result[k] = result[k] || defs[k];
-            }
-            return result;
-        };
-    };
-}
 
 declare interface Renderable {
     get element(): Element;
@@ -73,7 +60,7 @@ export class Entity implements PropertyAccessor, Renderable {
             const propType = `<span class="entity-property-type">${property.type}</span>`;
             return `<tr><td data-property="${property.name}">${propName}: ${propType}</td></tr>`;
         });
-        this.elem.innerHTML = `<thead><tr><th>${descriptor.name} <span class="toggle"></span></th></tr></thead><tbody>` + rows.join("") + "</tbody>";
+        this.elem.innerHTML = `<thead><tr><th>${descriptor.name} <span class="toggle"></span></th></tr></thead><tbody class="hidden">` + rows.join("") + "</tbody>";
 
         const toggle = this.elem.querySelector("thead>tr>th>span.toggle")!;
         const body = this.elem.querySelector("tbody")!;
@@ -103,218 +90,6 @@ export class Entity implements PropertyAccessor, Renderable {
 
 export class EntityRelationship {
     constructor(public source: EntityElement, public target: EntityElement) { }
-}
-
-/** A function that returns if two elements are connected. */
-type ConnectionCheckerFunction = (elem1: HTMLElement, elem2: HTMLElement) => boolean;
-
-declare interface ElasticLayoutOptions {
-    /** Charge of a object, repulsive force exerted by the object, according to Coulomb's law. */
-    readonly charge: number;
-    /** Stiffness of the spring, attractive force exerted by the connection between objects, according to Hooke's Law. */
-    readonly stiffness: number;
-    /** Drag force, acts in opposite direction to object's velocity. */
-    readonly drag: number;
-    /** Gravitational force that pulls objects towards the center. */
-    readonly gravity: number;
-    /** Number of initial iterations. */
-    readonly iterations: number;
-}
-
-/**
- * An object upon which forces act in a spring/charge system.
- */
-class ElasticLayoutObject {
-    force = new Vector(0, 0);
-    velocity = new Vector(0, 0);
-    position = new Vector(0, 0);
-    size = new Size(0, 0);
-
-    constructor(public element: HTMLElement) { }
-
-    public get mass(): number {
-        return 1;
-    }
-}
-
-/**
- * Produces layout with the help of a spring/charge system.
- *
- * Springs exert an attractive force, pulling connected elements towards one another. Charges exert a repulsive force,
- * pushing elements away from one another. A gravitational force pulls objects towards the center. Drag slows down
- * objects that move at high speeds.
- */
-class ElasticLayout {
-    private objects: ElasticLayoutObject[];
-    private connectivity: BooleanMatrix;
-    private lastTimestamp: DOMHighResTimeStamp = 0;
-    private running: boolean = false;
-
-    constructor(private options: ElasticLayoutOptions, private canvas: HTMLElement, elements: HTMLElement[], isConnectedFunc: ConnectionCheckerFunction) {
-        withDefaults<ElasticLayoutOptions>()({
-            charge: 1000,
-            stiffness: 0.2,
-            drag: 0.5,
-            gravity: 2.0,
-            iterations: 250,
-        });
-
-        this.objects = elements.map(element => {
-            return new ElasticLayoutObject(element);
-        });
-
-        this.connectivity = new BooleanMatrix(elements.length, elements.length);
-        for (let [i, source] of this.objects.entries()) {
-            for (let [j, target] of this.objects.entries()) {
-                this.connectivity.set(i, j, isConnectedFunc(source.element, target.element));
-            }
-        }
-    }
-
-    private getCenterPosition(): Vector {
-        const containerRect = this.canvas.getBoundingClientRect();
-        return new Vector(containerRect.width / 2, containerRect.height / 2);
-    }
-
-    /**
-     * Produces a list of objects that participate in elastic layout.
-     */
-    private getDynamicObjects(): ElasticLayoutObject[] {
-        return this.objects.filter(obj => {
-            return (obj.element.style.getPropertyPriority("left") != "important" && obj.element.style.getPropertyPriority("top") != "important");
-        });
-    }
-
-    /**
-     * Reads position information from HTML layout into internal data structures.
-     */
-    private loadPosition(objects: ElasticLayoutObject[]): void {
-        const containerRect = this.canvas.getBoundingClientRect();
-        const containerRef = new Point(containerRect.x, containerRect.y);
-
-        objects.forEach(obj => {
-            const rect = getOffsetRect(obj.element, containerRef);
-            obj.position = Vector.from(rect.center);
-            obj.size = new Size(rect.width, rect.height);
-        });
-    }
-
-    /**
-     * Sets position information in HTML layout based on data in internal data structures.
-     */
-    private savePosition(objects: ElasticLayoutObject[]): void {
-        objects.forEach(obj => {
-            // update position (unless an explicit value is forced with !important)
-            obj.element.style.left = (obj.position.x - obj.size.width / 2) + "px";
-            obj.element.style.top = (obj.position.y - obj.size.height / 2) + "px";
-        });
-    }
-
-    private isConnected(source: ElasticLayoutObject, target: ElasticLayoutObject) {
-        const sourceIndex = this.objects.indexOf(source);
-        const targetIndex = this.objects.indexOf(target);
-        return this.connectivity.get(sourceIndex, targetIndex);
-    }
-
-    initialize(): void {
-        const center = this.getCenterPosition();
-        const objects = this.getDynamicObjects();
-        this.loadPosition(objects);
-        this.step(center, objects, 0.1);
-        for (let k = 0; k < this.options.iterations; ++k) {
-            this.step(center, objects, 0.1);
-        }
-        this.savePosition(objects);
-        this.running = false;
-
-        this.start();
-    }
-
-    start(): void {
-        this.running = true;
-        this.lastTimestamp = performance.now();
-        window.requestAnimationFrame(this.tick.bind(this));
-    }
-
-    stop(): void {
-        this.running = false;
-    }
-
-    /**
-     * Adjusts position of objects over time as various forces act on them.
-     * @param centerPos The central position of the viewport towards which gravity pulls objects.
-     * @param objects List of objects that participate in elastic layout.
-     * @param elapsed A time delta that elapsed since the last invocation.
-     */
-    private step(centerPos: Vector, objects: ElasticLayoutObject[], elapsed: DOMHighResTimeStamp): void {
-        objects.forEach(obj => {
-            let offset = obj.velocity.times(elapsed);
-            obj.position.add(offset);
-        });
-        objects.forEach(obj => {
-            obj.velocity = obj.force.times(elapsed / obj.mass);
-        });
-
-        for (let i = 0; i < objects.length; ++i) {
-            const source = objects[i]!;
-
-            // attraction to center
-            const centerDir = centerPos.minus(source.position).normalize();
-            const centerForce = centerDir.times(this.options.gravity);
-            source.force.add(centerForce);
-
-            // drag force to dampen speed of fast-moving objects
-            const dragForce = source.velocity.times(-this.options.drag);
-            source.force.add(dragForce);
-
-            for (let j = i + 1; j < objects.length; ++j) {
-                const target = objects[j]!;
-
-                const vector = target.position.minus(source.position);
-                const distance = vector.magnitude();
-                vector.normalize();
-
-                // repulsive force between close items
-                const repulsiveForce = vector.times((this.options.charge * this.options.charge) / (1.0 + distance * distance));
-                source.force.add(repulsiveForce.reversed());
-                target.force.add(repulsiveForce);
-
-                // attractive force between connected items
-                if (this.isConnected(source, target)) {
-                    const attractiveForce = vector.times(this.options.stiffness * distance);
-                    source.force.add(attractiveForce);
-                    target.force.add(attractiveForce.reversed());
-                }
-            }
-        }
-    }
-
-    /**
-     * Invoked when an animation step is about to be painted.
-     * @param timestamp The timestamp received for an animation frame event.
-     */
-    private tick(timestamp: DOMHighResTimeStamp) {
-        let elapsed = (timestamp - this.lastTimestamp) / 1000;
-        if (elapsed > 5.0) {
-            elapsed = 5.0;
-        }
-
-        const center = this.getCenterPosition();
-        const objects = this.getDynamicObjects();
-        this.loadPosition(objects);
-        while (elapsed > 0.1) {
-            this.step(center, objects, 0.1);
-            elapsed -= 0.1;
-        }
-
-        this.step(center, objects, elapsed);
-        this.savePosition(objects);
-
-        if (this.running) {
-            this.lastTimestamp = timestamp;
-            window.requestAnimationFrame(this.tick.bind(this));
-        }
-    }
 }
 
 declare interface EntityRelationshipData {
