fc8da307f6c65a549631927d0556ab6d1d4c8183
Levente Hunyadi <hunyadi@gmail.com>
2024-09-13 11:46:28 +0200

    Update license

--- src/erdiagram.ts
+++ src/erdiagram.ts
@@ -2,9 +2,9 @@
  * Entity Relationship Diagram (in TypeScript)
  * @author  Levente Hunyadi
  * @version 1.0
- * @remarks Copyright (C) 2022 Levente Hunyadi
+ * @remarks Copyright (C) 2022-2024 Levente Hunyadi
  * @remarks Licensed under MIT, see https://opensource.org/licenses/MIT
- * @see     https://hunyadi.info.hu/
+ * @see     https://github.com/hunyadi/entity-relationship-diagram/
  **/
 
 import { Arrow, DiagramCanvas } from "./diagram";
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack")
 
 module.exports = {
   mode: "production",
@@ -19,5 +20,21 @@ module.exports = {
         loader: "ts-loader"
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      raw: true,
+      banner: [
+        "/**",
+        " * Entity Relationship Diagram (in TypeScript)",
+        " * @author  Levente Hunyadi",
+        " * @version 1.0",
+        " * @remarks Copyright (C) 2022-2024 Levente Hunyadi",
+        " * @remarks Licensed under MIT, see https://opensource.org/licenses/MIT",
+        " * @see     https://github.com/hunyadi/entity-relationship-diagram/",
+        " **/"
+      ].join("\n"),
+      stage: webpack.Compilation.PROCESS_ASSETS_STAGE_REPORT,
+    })
+  ]
 };
